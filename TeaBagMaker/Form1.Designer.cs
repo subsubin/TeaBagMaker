067667c28e471c10d33f7ab2c9714dd8690ab662
Subin Kim <s2017s24@e-mirim.hs.kr>
2019-05-21 10:45:05 +0900

    #1 폼디자인(1)

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CList = new System.Windows.Forms.ComboBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "담그기!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(651, 429);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "2019-05-21  3603 김수빈";
+            // 
+            // CList
+            // 
+            this.CList.FormattingEnabled = true;
+            this.CList.Location = new System.Drawing.Point(27, 33);
+            this.CList.Name = "CList";
+            this.CList.Size = new System.Drawing.Size(121, 20);
+            this.CList.TabIndex = 2;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(27, 85);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(81, 12);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "결과가 여기에";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.CList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CList;
+        private System.Windows.Forms.Label lblResult;
     }
 }
 
--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -12,9 +12,21 @@ namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
+        string[] SList = new string[] {"홍차", "녹차", "루이보스차", "국화차"};
+        string orgStr = "";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < SList.Length; i++) {
+                this.CList.Items.Add(SList[i]);
+            }
+            this.orgStr = lblResult.Text;
+            if (CList.Items.Count > 0)
+                this.CList.SelectedIndex = 0;
         }
     }
 }
